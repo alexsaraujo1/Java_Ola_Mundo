c5b0b925434f85b95305d310b304fdd1ffef9521
alexsaraujo1 <82852965+alexsaraujo1@users.noreply.github.com>
2023-02-13 22:20:15 -0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,23 @@
 class Hello {
     public static void main (String[] args) {
-        System.out.println("Hello, World.");
+    	int inteiro = 34;
+        boolean booleana = true;
+        char caracter = 'C';
+        char codigo = 56;
+        float flutuante = 0.700000f;
+        double dobro = 142.6f;
+        String palavra = "Foobar";
+        
+        if(inteiro <= 34){
+            palavra = "Verdadeiro";
+        }else{
+            palavra = "Falso";
+        }
+        
+        for(char i = 'a'; i != 'k'; i++){
+        	System.out.println(i);
+        }
+        
+        System.out.println(palavra + inteiro);
     }
 }
